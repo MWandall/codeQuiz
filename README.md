36d4066c388a681b27bf6767a634b2c349a0837e
MWandall <144394104+MWandall@users.noreply.github.com>
2023-09-24 21:42:21 -0500

    added imgs, notes for img credits, and basic css

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # codeQuiz
+
+
+whos that pokemon image cred :
+https://www.deviantart.com/amitlu89/art/Who-s-that-Pokemon-254910939
+ 
+amitlu89
+
+pkmon images: https://www.stickpng.com/cat/games/pokemon?page=2
+
+The products or characters depicted in these images are © by their respective authors.
---
+++ assets/script.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// **idea to link hide/show to html and CSS
+
+// ***Get references to the button and the element to toggle
+// var toggleButton = document.getElementById('toggleButton');
+// var elementToToggle = document.getElementById('elementToToggle');
+
+// *****Add a click event listener to the button
+// toggleButton.addEventListener('click', function() {
+// ****Check the current display style of the element
+//     var currentDisplayStyle = getComputedStyle(elementToToggle).display;
+
+//  ****Toggle the display property
+//     if (currentDisplayStyle === 'none') {
+// **If it's hidden, show it
+//         elementToToggle.style.display = 'block'; 
+//  **block just example
+//     } else {
+// **If it's shown, hide it
+//         elementToToggle.style.display = 'none';
+//     }
+// });
+
+
+
+
+//* AS A coding boot camp student
+// *I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// *SO THAT I can gauge my progress compared to my peers
+
+
+// *GIVEN I am taking a code quiz
+// *WHEN I click the start button
+// *THEN a timer starts and I am presented with a question
+// *WHEN I answer a question
+// *THEN I am presented with another question
+// *WHEN I answer a question incorrectly
+// *THEN time is subtracted from the clock
+// *WHEN all questions are answered or the timer reaches 0
+// *THEN the game is over
+// *WHEN the game is over
+// *THEN I can save my initials and my score
